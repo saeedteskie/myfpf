d05f4b6900b9c4edcb4f6f04246f0cf26106e2f0
saeedteskie <38921870+saeedteskie@users.noreply.github.com>
2026-06-25 09:05:57 -0400

    Delete saeed_customizations/rc_call_hooks.py

--- saeed_customizations/rc_call_hooks.py
+++
@@ -1,8 +0,0 @@
-import frappe
-
-def after_save_rc_call(doc, method):
-    # Your custom logic here
-    # frappe.logger().info(f"rc_call saved with ID: {doc.name}")
-    frappe.publish_realtime('sales_center_rc_call_socket', {'rc_call': doc, "method": method});
-    # frappe.msgprint("Hi there");
-    # print("hello");
